abcf9fa5bbc09672288b1bbf301255548d131887
Jenny Fong <jenny.fong@nyumc.org>
2019-05-01 16:12:38 -0400

    remove old ver of mysql in migration

--- src/server/database/migrations/20190501195057-init.sql
+++ src/server/database/migrations/20190501195057-init.sql
@@ -1,29 +1,33 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.7.25)
-# Database: parkData
-# Generation Time: 2019-04-11 14:27:00 +0000
-# ************************************************************
+-- -------------------------------------------------------------
+-- TablePlus 2.3(222)
+--
+-- https://tableplus.com/
+--
+-- Database: parkData
+-- Generation Time: 2019-05-01 16:11:40.0930
+-- -------------------------------------------------------------
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-# Dump of table operatingHour
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `operatingHour`;
+CREATE TABLE `migrations` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  `sha1sum` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `executedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `operatingHour` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -39,14 +43,7 @@ CREATE TABLE `operatingHour` (
   PRIMARY KEY (`id`),
   KEY `operatinghours_ibfk_1` (`parkId`),
   CONSTRAINT `operatinghour_ibfk_1` FOREIGN KEY (`parkId`) REFERENCES `park` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table park
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `park`;
+) ENGINE=InnoDB AUTO_INCREMENT=519 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `park` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -55,14 +52,7 @@ CREATE TABLE `park` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table ride
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ride`;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ride` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -75,14 +65,7 @@ CREATE TABLE `ride` (
   PRIMARY KEY (`id`),
   KEY `ride_ibfk_1` (`parkId`),
   CONSTRAINT `ride_ibfk_1` FOREIGN KEY (`parkId`) REFERENCES `park` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table waitTime
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `waitTime`;
+) ENGINE=InnoDB AUTO_INCREMENT=292 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `waitTime` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -98,14 +81,16 @@ CREATE TABLE `waitTime` (
   PRIMARY KEY (`id`),
   KEY `waitTimes_ibfk_2` (`rideId`),
   CONSTRAINT `waitTimes_ibfk_2` FOREIGN KEY (`rideId`) REFERENCES `ride` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1170633 DEFAULT CHARSET=utf8;
 
 
 
 
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
